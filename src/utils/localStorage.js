e23847207ffef91fd369690872ffab5580c7c178
JakkaGopalaKrishna <jakkakrishna2003@gmail.com>
2026-05-22 14:31:07 +0530

    feat:add  getUserId function

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -9,3 +9,17 @@ export const getToken = () => {
 export const removeToken = () => {
   localStorage.removeItem("token");
 };
+
+export const getUserId = () => {
+  const token = getToken();
+
+  // 1. Split the token and grab the payload (middle part)
+  const base64Url = token.split(".")[1];
+
+  // 2. Fix base64url padding replacements
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+
+  // 3. Decode base64 to string and parse JSON
+  const response = JSON.parse(atob(base64));
+  return response.user_id;
+};
